821e3143d29b3b3a7a01dea81d0fbb32abe090d8
Alex H <cvcd005@gmail.com>
2020-02-29 10:55:53 +0300

    Updates

---
+++ precache-manifest.161763a3aa26647380ed78f404e77cdd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d0b8b33db5bb5b3eb11e8c338831d8",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "dda001af71c3c16ffca1",
+    "url": "/blog/static/css/2.03d7f783.chunk.css"
+  },
+  {
+    "revision": "afc82d76469bffb965e0",
+    "url": "/blog/static/css/main.486044ff.chunk.css"
+  },
+  {
+    "revision": "dda001af71c3c16ffca1",
+    "url": "/blog/static/js/2.3228be96.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/blog/static/js/2.3228be96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afc82d76469bffb965e0",
+    "url": "/blog/static/js/main.b8e1d6ce.chunk.js"
+  },
+  {
+    "revision": "34c7b270b978cd13aad9",
+    "url": "/blog/static/js/runtime-main.fb2521da.js"
+  },
+  {
+    "revision": "811c8baeca3bb3329488f729979c1fbc",
+    "url": "/blog/static/media/opensans.811c8bae.woff"
+  },
+  {
+    "revision": "6f05808b4f6a11d3feb1716291c7c038",
+    "url": "/blog/static/media/opensansbold.6f05808b.woff"
+  }
+]);
