c801c7a7dc2899b998eb69f56270d6541dfc04ae
Alex H <cvcd005@gmail.com>
2020-03-03 07:49:11 +0300

    change axios config

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,17 +1,20 @@
 import axios from 'axios';
 
+const axiosInstance = axios.create({
+  baseURL: 'https://conduit.productionready.io/api/',
+});
+
 const API_URLS = {
-  BASE_URl: 'https://conduit.productionready.io/api/',
   LOGIN_USER: 'users/login',
   REGISTER_USER: 'users',
 };
 
 export const signUp = async user => {
-  return axios.post(`${API_URLS.BASE_URl}${API_URLS.REGISTER_USER}`, user);
+  return axiosInstance.post(`${API_URLS.REGISTER_USER}`, user);
 };
 
 export const signIn = async user => {
-  return axios.post(`${API_URLS.BASE_URl}${API_URLS.LOGIN_USER}`, user);
+  return axiosInstance.post(`${API_URLS.LOGIN_USER}`, user);
 };
 
 export const createLocalStorage = usr => {
