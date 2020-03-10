60a1c9bca6cfbe151efb309248b971bcf7f4cdf5
Alex H <cvcd005@gmail.com>
2020-03-10 08:39:06 +0300

    add pagination to List Articles

--- src/Articles/ListArticles.jsx
+++ src/Articles/ListArticles.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Pagination } from 'antd';
 
 import { getArticlesList } from '../Api/Api';
 import { actionCreatorsArticlesList } from '../Store/actions';
@@ -10,15 +11,23 @@ class ListArticles extends React.Component {
   async componentDidMount() {
     const response = await getArticlesList();
     const { addArticlesList } = this.props;
-    console.log(response.data.articles);
+    addArticlesList(response.data.articles);
+  }
+
+  onChange = async (evt) => {
+    const response = await getArticlesList(evt * 10 - 10);
+    const { addArticlesList } = this.props;
     addArticlesList(response.data.articles);
   }
 
   render () {
     const { articlesList } = this.props;
     return (
-    <div className="articles-list">
-      {articlesList.map(el => <LiteArticle article={el} key={el.slug} />) }
+    <div>
+      <div className="articles-list">
+        {articlesList.map(el => <LiteArticle article={el} key={el.slug} />) }
+      </div>
+      <Pagination total={500} onChange={this.onChange} className="pagination"/>
     </div>
     )
   }
